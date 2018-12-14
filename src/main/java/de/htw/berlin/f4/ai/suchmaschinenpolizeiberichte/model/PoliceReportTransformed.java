a49c707249f7138f4c49e0a29af3ba32dbb59057
Mayk Akifovski <s0558208@htw-berlin.de>
2018-12-14 13:39:25 +0100

    Model optimized

--- src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/model/PoliceReportTransformed.java
+++ src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/model/PoliceReportTransformed.java
@@ -4,7 +4,6 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDate;
 import java.util.List;
 
 
@@ -18,8 +17,8 @@ public class PoliceReportTransformed {
     @Id
     private String id;
     private String idToOrigin;
-    private String location;
-    private LocalDate date;
+    private List<String> location;
+    private long date;
     private List<String> title;
     private List<String> content;
 }
