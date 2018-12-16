10c28c7179278c164bec52154c5336fed05707bb
Mayk Akifovski <s0558208@htw-berlin.de>
2018-12-16 23:39:24 +0100

    Lematization implemented, working not perfectly, will be fixed

--- src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/model/PoliceReport.java
+++ src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/model/PoliceReport.java
@@ -1,9 +1,15 @@
 package de.htw.berlin.f4.ai.suchmaschinenpolizeiberichte.model;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "PoliceReports")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
 public class PoliceReport {
     @Id
     public String id;
@@ -15,72 +21,4 @@ public class PoliceReport {
     public String createdAt;
     public String content;
 
-    public PoliceReport() {
-    }
-
-    public PoliceReport(String id, String header, Boolean isLocationInHeader, String title, String url, String createdAt, String content) {
-        this.id = id;
-        this.header = header;
-        this.isLocationInHeader = isLocationInHeader;
-        this.title = title;
-        this.url = url;
-        this.createdAt = createdAt;
-        this.content = content;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getHeader() {
-        return header;
-    }
-
-    public Boolean getLocationInHeader() {
-        return isLocationInHeader;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setHeader(String header) {
-        this.header = header;
-    }
-
-    public void setLocationInHeader(Boolean locationInHeader) {
-        isLocationInHeader = locationInHeader;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
--- src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/services/PoliceReportTransformer.java
+++ src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/services/PoliceReportTransformer.java
@@ -7,10 +7,14 @@ import de.htw.berlin.f4.ai.suchmaschinenpolizeiberichte.repository.PoliceReportT
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -26,9 +30,11 @@ public class PoliceReportTransformer {
     private PoliceReportTransformedRepository policeReportTransformedRepository;
 
     private List<String> filter;
+    private HashMap<String, String> lexikon;
 
     public void run() throws IOException {
         filter = initFilter();
+        initLexikon();
 
         List<PoliceReport> policeReports = policeReportRepository.getByIsLocationInHeader(true);
 
@@ -42,8 +48,28 @@ public class PoliceReportTransformer {
     }
 
     private List<String> initFilter() throws IOException {
-        try (Stream<String> stream = Files.lines(Paths.get("/home/leoroi/hdd/0/ses/suchmaschinen-polizei-berichte/src/main/resources/stopwords.txt"))) {
+        try (Stream<String> stream = Files.lines(Paths.get("D:\\HTWBerlin\\Semester5\\SuchMaschinen\\suchmaschinen-polizei-berichte\\src\\main\\resources\\stopwords.txt"))) {
             return stream.collect(Collectors.toList());
+        }
+    }
+
+    private void initLexikon() throws IOException {
+        lexikon = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("D:\\HTWBerlin\\Semester5\\SuchMaschinen\\suchmaschinen-polizei-berichte\\src\\main\\resources\\delexicon.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // process the line.
+                String[] s = line.split("\t");
+                String key = s[0].toLowerCase();
+                String[] values = s[4].split(".");
+                String value;
+                if (values.length == 0) {
+                    value = s[4].toLowerCase();
+                } else {
+                    value = values[values.length - 1].toLowerCase();
+                }
+                lexikon.put(key, value);
+            }
         }
     }
 
@@ -51,15 +77,16 @@ public class PoliceReportTransformer {
         PoliceReportTransformed policeReportTransformed = new PoliceReportTransformed();
         policeReportTransformed.setIdToOrigin(policeReport.id);
         policeReportTransformed.setContent(tokenize(policeReport.content));
+        policeReportTransformed.setTitle(tokenize(policeReport.title));
         extractDateLocation(policeReport.header, policeReportTransformed);
         return policeReportTransformed;
     }
 
     /**
      * pol_df["Location"] = pol_df["Header"].str.extract(
-     *    r"""(Mitte|Friedrichshain *- *Kreuzberg|Pankow|Charlottenburg* - *Wilmersdorf|Spandau|Steglitz* - *Zehlendorf|Tempelhof* - *Schöneberg|Neukölln|Treptow* - *Köpenick|Marzahn* - *Hellersdorf|Lichtenberg|Reinickendorf|bezirksübergreifend|berlinweit|bundesweit)""")
+     * r"""(Mitte|Friedrichshain *- *Kreuzberg|Pankow|Charlottenburg* - *Wilmersdorf|Spandau|Steglitz* - *Zehlendorf|Tempelhof* - *Schöneberg|Neukölln|Treptow* - *Köpenick|Marzahn* - *Hellersdorf|Lichtenberg|Reinickendorf|bezirksübergreifend|berlinweit|bundesweit)""")
      * pol_df.loc[pol_df["Location"].isnull(),"Location"] = pol_df[pol_df["Location"].isnull()]["Content"].str.extract(
-     *    r"""(Mitte|Friedrichshain *- *Kreuzberg|Pankow|Charlottenburg* - *Wilmersdorf|Spandau|Steglitz* - *Zehlendorf|Tempelhof* - *Schöneberg|Neukölln|Treptow* - *Köpenick|Marzahn* - *Hellersdorf|Lichtenberg|Reinickendorf|bezirksübergreifend|[bB]erlinweit|Berlin/Brandenburg|Berlin/Köln|Berlin/Bayern|Berlin/Niedersachsen/Niederlande|bundesweit)""", expand=False)
+     * r"""(Mitte|Friedrichshain *- *Kreuzberg|Pankow|Charlottenburg* - *Wilmersdorf|Spandau|Steglitz* - *Zehlendorf|Tempelhof* - *Schöneberg|Neukölln|Treptow* - *Köpenick|Marzahn* - *Hellersdorf|Lichtenberg|Reinickendorf|bezirksübergreifend|[bB]erlinweit|Berlin/Brandenburg|Berlin/Köln|Berlin/Bayern|Berlin/Niedersachsen/Niederlande|bundesweit)""", expand=False)
      * pol_df["Location"] = pol_df["Location"].str.replace(" ","")
      *
      * @param header
@@ -69,13 +96,31 @@ public class PoliceReportTransformer {
         String[] s = header.split(" ");
         System.out.println();
 
+        List<String> location = Collections.emptyList();
+        policeReportTransformed.setLocation(location);
+        long date = 0;
+        policeReportTransformed.setDate(date);
     }
 
     private List<String> tokenize(String toSplit) {
         return Arrays.stream(toSplit.split(" "))
                 .map(this::transformWord)
-                .filter(word -> !filter.contains(word) || !isEmpty(word))
+                .filter(this::toFilter)
+                .map(word -> lexikon.getOrDefault(word, word))
                 .collect(Collectors.toList());
+    }
+
+    private boolean toFilter(String word) {
+        return !(filter.contains(word) || isEmpty(word) || isNumber(word));
+    }
+
+    private boolean isNumber(String word) {
+        try {
+            Double.parseDouble(word);
+            return true;
+        } catch (Exception ignored) {
+            return false;
+        }
     }
 
     private String transformWord(String word) {
