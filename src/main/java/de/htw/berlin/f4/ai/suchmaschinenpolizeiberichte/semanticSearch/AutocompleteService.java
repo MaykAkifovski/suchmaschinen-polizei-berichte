a2c6836622719da4d20bf985dfec66fe31cacd33
Claudio Vindimian <s0558279@htw-berlin.de>
2019-02-04 21:59:54 +0100

    levensthein distance for autocomplete implemented

--- src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/semanticSearch/AutocompleteService.java
+++ src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/semanticSearch/AutocompleteService.java
@@ -3,6 +3,8 @@ package de.htw.berlin.f4.ai.suchmaschinenpolizeiberichte.semanticSearch;
 import de.htw.berlin.f4.ai.suchmaschinenpolizeiberichte.repository.SuggestionWordsLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.apache.commons.text.similarity.LevenshteinDistance;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -15,20 +17,57 @@ public class AutocompleteService {
 
 
     public List<String> getSuggestions(String sb, int n) {
-
-        return getTopNFSortedsuggestions(sb.toLowerCase(), n);
+        try {
+            List<String> suggestions = getTopNFSortedsuggestions(sb.toLowerCase(), n);
+            if (suggestions.size() == 0) {
+                suggestions = getTopNFSortedsuggestionsWithLevensthein(sb.toLowerCase(), n);
+            }
+            return suggestions;
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
     }
 
-    private List<String> getTopNFSortedsuggestions(String sb, int n){
-        try{
-            Map<String,Integer> predictedWords =  suggestionWordsLoader.getTrie().getSuggestions(sb);
-            return predictedWords.entrySet().stream()
-                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                    .limit(n).map(x->x.getKey())
-                    .collect(Collectors.toList());
-        } catch(Exception e){
+    private List<String> getTopNFSortedsuggestions(String sb, int n) {
+
+        if (sb.length() == 0) {
             return Collections.emptyList();
         }
 
+        try {
+            Map<String, Integer> predictedWords = suggestionWordsLoader.getTrie().getSuggestions(sb);
+            return predictedWords.entrySet().stream()
+                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                    .limit(n).map(x -> x.getKey())
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
     }
+
+    private List<String> getTopNFSortedsuggestionsWithLevensthein(String sb, int n) {
+        Map<String, Integer> predictedWords = suggestionWordsLoader.getTrie().getSuggestions(sb.substring(0, 2));
+        List<String> predictedWordsList = new ArrayList<>(predictedWords.keySet());
+
+        String candidate = findCandidate(predictedWordsList, sb, 2);
+        if (candidate.length() == 0) {
+            candidate = findCandidate(predictedWordsList, sb, 3);
+        }
+        return getTopNFSortedsuggestions(candidate, n);
+    }
+
+    private String findCandidate(List<String> predictedWordsList, String sb, int levDis) {
+        LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
+        String candidate = "";
+        for (String bar : predictedWordsList) {
+            bar = StringUtils.substring(bar, 0, sb.length());
+
+            if (levenshteinDistance.apply(bar, sb) < levDis) {
+                candidate = bar;
+                break;
+            }
+        }
+        return candidate;
+    }
+
 }
