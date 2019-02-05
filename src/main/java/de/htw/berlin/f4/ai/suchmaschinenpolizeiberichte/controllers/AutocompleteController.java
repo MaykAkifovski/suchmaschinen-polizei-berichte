2e81ec19b9c1025e415741293f388174388e8d43
Claudio Vindimian <s0558279@htw-berlin.de>
2019-02-05 23:23:32 +0100

    autocomplete for more word allowed

--- src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/controllers/AutocompleteController.java
+++ src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/controllers/AutocompleteController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -22,7 +26,23 @@ public class AutocompleteController {
 
     @RequestMapping(method = GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<String> getSearch(@RequestParam("sb") String subString, @RequestParam("size") int n) {
-        return autocompleteService.getSuggestions(subString, n);
+        if (!subString.contains(" ")) {
+            return autocompleteService.getSuggestions(subString, n);
+        }
+
+        List<String> tokenized = new LinkedList<>(Arrays.asList(subString.split("\\s")));
+
+        if (tokenized.size() < 2) {
+            return Collections.emptyList();
+
+        }
+
+        List<String> results = autocompleteService
+                .getSuggestions(tokenized.get(tokenized.size() - 1), n);
+        tokenized.remove(tokenized.size() - 1);
+        return results.stream()
+                .map(x -> tokenized.stream().collect(Collectors.joining(" ")) + " " + x)
+                .collect(Collectors.toList());
     }
 
 
