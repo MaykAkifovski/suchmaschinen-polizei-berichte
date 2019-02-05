bcdbcd7d6f42152d4f63b1f17b1f4a05436fb4bc
Mayk Akifovski <s0558208@htw-berlin.de>
2019-02-05 14:50:25 +0100

    - Fasttext per Controller accessible

---
+++ src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/controllers/FasttextController.java
@@ -0,0 +1,26 @@
+package de.htw.berlin.f4.ai.suchmaschinenpolizeiberichte.controllers;
+
+import de.htw.berlin.f4.ai.suchmaschinenpolizeiberichte.repository.FasttextLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping("/dbTaskController")
+public class FasttextController {
+
+    @Autowired
+    private FasttextLoader fasttextLoader;
+
+    @RequestMapping(method = GET)
+    public List<Map.Entry<String, Double>> getSuggestions(
+            @RequestParam(value = "word", defaultValue = "Auto") String word) {
+        return fasttextLoader.cosinusSimilarity(word);
+    }
+}
