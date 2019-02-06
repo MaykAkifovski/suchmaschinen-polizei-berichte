96bfa277eac15b1d1504033c03ead3b38a849b18
Claudio Vindimian <s0558279@htw-berlin.de>
2019-02-06 12:48:54 +0100

    some bad last vocabulary cleaning

--- src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/repository/SuggestionWordsLoader.java
+++ src/main/java/de/htw/berlin/f4/ai/suchmaschinenpolizeiberichte/repository/SuggestionWordsLoader.java
@@ -14,40 +14,46 @@ public class SuggestionWordsLoader {
     private Trie trie = new Trie();
 
     @PostConstruct
-    public void init()  {
-        try
-        {
+    public void init() {
+        try {
             InputStream fis = getClass().getClassLoader().getResourceAsStream("suggestionWords2.ser");
             ObjectInputStream ois = new ObjectInputStream(fis);
             suggestionWords = (HashMap) ois.readObject();
             ois.close();
             fis.close();
-        }catch(IOException ioe)
-        {
+        } catch (IOException ioe) {
             ioe.printStackTrace();
             return;
-        }catch(ClassNotFoundException c)
-        {
+        } catch (ClassNotFoundException c) {
             System.out.println("Class not found for Deserialization");
             c.printStackTrace();
             return;
         }
         System.out.println("Deserialized HashMap..");
 
+        lastCleaning("e");
+        lastCleaning("en");
+        lastCleaning("nen");
+        lastCleaning("n");
+
         suggestionWords.entrySet().forEach(x -> {
-            trie.insert(x.getKey(),x.getValue());
-      });
+            trie.insert(x.getKey(), x.getValue());
+        });
         System.out.println("Trie loaded..");
         suggestionWords = null;
     }
 
-
-
+private void lastCleaning(String wordEnd) {
+    Map<String, Integer> results = new HashMap<>(suggestionWords);
+    for (Map.Entry<String, Integer> entry : results.entrySet()) {
+        String singular = entry.getKey();
+        if (suggestionWords.containsKey(singular + wordEnd) && suggestionWords.containsKey(singular)) {
+            suggestionWords.put(singular, suggestionWords.get(singular+wordEnd) + suggestionWords.get(singular));
+            suggestionWords.remove(singular + wordEnd);
+        }
+    }
+}
     public Trie getTrie() {
         return trie;
     }
-
-    }
-
-
-
+}
